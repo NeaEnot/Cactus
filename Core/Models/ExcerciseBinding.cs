d5f10a8994d16b882a50c3f63d621d392a8fa7dd
NeaEnot <oleg.kozloss@yandex.ru>
2021-05-28 11:32:07 +0400

    Binding модель упражнения состоящая из id, полного наименования, вопроса и ответа

---
+++ Core/Models/ExcerciseBinding.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.Models
+{
+	/// <include file='Documentation.xml' path='documentation/members[@name="ExcerciseBinding"]/ExcerciseBinding/*'/>
+	public class ExcerciseBinding
+	{
+		/// <include file='Documentation.xml' path='documentation/members[@name="ExcerciseBinding"]/Id/*'/>
+		public int? Id { get; set; }
+
+		/// <include file='Documentation.xml' path='documentation/members[@name="ExcerciseBinding"]/Title/*'/>
+		public string Title { get; set; }
+
+		/// <include file='Documentation.xml' path='documentation/members[@name="ExcerciseBinding"]/Question/*'/>
+		public string Question { get; set; }
+
+		/// <include file='Documentation.xml' path='documentation/members[@name="ExcerciseBinding"]/Answrer/*'/>
+		public string Answrer { get; set; }
+	}
+}
--- Tests/CoreTest/ModelsTest.cs
+++ Tests/CoreTest/ModelsTest.cs
@@ -6,7 +6,7 @@ namespace Tests.CoreTest
 	public class ModelsTest
 	{
 		[Fact]
-		public void TestExcercise()
+		public void TestExcerciseView()
 		{
 			ExcerciseView model = new ExcerciseView { Id = 1, Title = "Tests.CoreTest.Test", Question = "It is a test question", Answrer = "It is a test answer" };
 
@@ -14,6 +14,21 @@ namespace Tests.CoreTest
 			Assert.Equal("Tests.CoreTest.Test", model.Title);
 			Assert.Equal("It is a test question", model.Question);
 			Assert.Equal("It is a test answer", model.Answrer);
+		}
+		[Fact]
+		public void TestExcerciseBinding()
+		{
+			ExcerciseBinding model1 = new ExcerciseBinding { Id = 1, Title = "Tests.CoreTest.Test", Question = "It is a test question", Answrer = "It is a test answer" };
+			ExcerciseBinding model2 = new ExcerciseBinding { Id = null, Title = "Tests.CoreTest.Test", Question = "It is a test question", Answrer = "It is a test answer" };
+
+			Assert.Equal(1, model1.Id);
+			Assert.Equal("Tests.CoreTest.Test", model1.Title);
+			Assert.Equal("It is a test question", model1.Question);
+			Assert.Equal("It is a test answer", model1.Answrer);
+			Assert.Null(model2.Id);
+			Assert.Equal("Tests.CoreTest.Test", model2.Title);
+			Assert.Equal("It is a test question", model2.Question);
+			Assert.Equal("It is a test answer", model2.Answrer);
 		}
 	}
 }
